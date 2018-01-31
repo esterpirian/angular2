a2a90770081658843dce22eac7c51031890aef58
esterpirian <35893829+esterpirian@users.noreply.github.com>
2018-01-31 08:21:57 +0000

    Create main.model.ts

---
+++ App/com/main.model.ts
@@ -0,0 +1,11 @@
+export class MainModel {
+   public userName: string;
+    public password: string;
+    constructor() {
+        this.userName = "u41";
+        this.password = "o99ooo";
+    }
+    updateLoginData(data) {
+
+    }
+}
